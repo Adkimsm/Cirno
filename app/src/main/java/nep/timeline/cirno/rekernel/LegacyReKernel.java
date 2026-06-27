1f322a32209cdf3a6e48afcdf175061a8f72274d
Adkimsm <adkinsm9277@gmail.com>
2026-06-28 00:39:39 +0800

    fix(rekernel): add missing FileDescriptor import in LegacyReKernel

--- app/src/main/java/nep/timeline/cirno/rekernel/LegacyReKernel.java
+++ app/src/main/java/nep/timeline/cirno/rekernel/LegacyReKernel.java
@@ -6,6 +6,7 @@ import android.system.Os;
 import android.system.OsConstants;
 
 import java.io.File;
+import java.io.FileDescriptor;
 import java.io.InterruptedIOException;
 import java.net.SocketAddress;
 import java.nio.ByteBuffer;
